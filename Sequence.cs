42a93860a3498f08d270bedff9d4afcf0b30d791
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-02-11 17:03:53 -0800

    simplified Sequence

--- Sequence.cs
+++ Sequence.cs
@@ -4,20 +4,20 @@ using System.Collections.Generic;
 namespace KevinDOMara.SDSU.CS657.Assignment1
 {
     /// <summary>
-    /// Specifies a pattern of Directions for traversal on a grid.
+    /// Specifies a pattern of Direction for traversal on a grid.
     /// </summary>
     public class Sequence
     {
         public enum Mode { Repeat, Terminate }
         public enum Type { Movement, Sonar }
 
-        public readonly List<Direction> directions;
+        public readonly Direction direction;
         public readonly Mode mode;
         public readonly Type type;
 
-        public Sequence(List<Direction> directions, Mode mode, Type type)
+        public Sequence(Direction direction, Mode mode, Type type)
         {
-            this.directions = directions;
+            this.direction = direction;
             this.mode = mode;
             this.type = type;
         }

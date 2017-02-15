c8bb7baec61b34ef3ac1f5d718a0dee2ba346540
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-02-15 12:38:34 -0800

    [+] framework for A* Search

---
+++ AStarSearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KevinDOMara.SDSU.CS657.Assignment1
+{
+    /// <summary>
+    /// A* Search modified to include restrictions on the Rover's allowed
+    /// movement and rotations.
+    /// </summary>
+    public class AStarSearch
+    {
+        public readonly Grid grid;
+        public readonly Vector2 start;
+        public readonly Vector2 goal;
+        public readonly Direction startDirection;
+
+        private Node[,] nodes;
+
+        public AStarSearch(Grid grid, Vector2 start, Direction startDirection,
+            Vector2 goal)
+        {
+            this.grid = grid;
+            this.start = start;
+            this.goal = goal;
+            this.startDirection = startDirection;
+            Search();
+        }
+
+        private void Search()
+        {
+
+        }
+
+        public Stack<Vector2> GetShortestPath()
+        {
+            var shortestPath = new Stack<Vector2>();
+
+            // push goal
+            // push (goal.closestPath), repeat
+            // stop @ start
+
+            return shortestPath;
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KevinDOMara.SDSU.CS657.Assignment1
+{
+    /// <summary>
+    /// Node of a graph. Holds a list of Path's leading to it.
+    /// </summary>
+    public class Node
+    {
+        public readonly Vector2 pos;
+        public List<Path> Paths { get; private set; }
+        Node(Vector2 position)
+        {
+            pos = position;
+            Paths = new List<Path>();
+        }
+
+        /// <summary>
+        /// Add a new Path leading to this Node.
+        /// </summary>
+        public void Append(Path path) { Paths.Add(path); }
+
+        /// <summary>
+        /// Replace all Paths leading to this Node with a single new path.
+        /// </summary>
+        public void Replace(Path path) { Paths = new List<Path> { path }; }
+    }
+
+    /// <summary>
+    /// Holds a single step leading to the owner's Node.
+    /// </summary>
+    public class Path
+    {
+        public readonly Vector2 from;
+        public readonly int cost;
+        public readonly Bearing facing;
+
+        public Path(Vector2 from, int cost, Bearing facing)
+        {
+            this.from = from;
+            this.cost = cost;
+            this.facing = facing;
+        }
+    }
+}

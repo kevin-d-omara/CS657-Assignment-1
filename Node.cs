a794c522d70ca772e2f09940cca716ee442f1592
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-02-16 08:54:40 -0800

    clarified comments

--- Node.cs
+++ Node.cs
@@ -5,7 +5,8 @@ using Priority_Queue;
 namespace KevinDOMara.SDSU.CS657.Assignment1
 {
     /// <summary>
-    /// Node of a graph. Holds a list of Path's leading to it.
+    /// Node of a graph. Holds a list of Path's leading to it. Note: all Paths
+    /// owned by a Node have the same cost and different facings.
     /// </summary>
     public class Node
     {
@@ -26,22 +27,5 @@ namespace KevinDOMara.SDSU.CS657.Assignment1
         /// Replace all Paths leading to this Node with a single new path.
         /// </summary>
         public void Replace(Path path) { Paths = new List<Path> { path }; }
-    }
-
-    /// <summary>
-    /// Holds a single step leading to the owner's Node.
-    /// </summary>
-    public class Path
-    {
-        public readonly Vector2 from;
-        public readonly int cost;
-        public readonly Bearing facing;
-
-        public Path(Vector2 from, int cost, Bearing facing)
-        {
-            this.from = from;
-            this.cost = cost;
-            this.facing = facing;
-        }
     }
 }

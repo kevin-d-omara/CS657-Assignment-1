a23c9cd328b47e5740402a612ab1cfcdb72ae002
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-02-16 08:54:27 -0800

    moved Path class to own file

---
+++ Path.cs
@@ -0,0 +1,19 @@
+﻿namespace KevinDOMara.SDSU.CS657.Assignment1
+{
+    /// <summary>
+    /// Holds a single step leading to the owner's Node.
+    /// </summary>
+    public class Path
+    {
+        public readonly Vector2 from;
+        public readonly int cost;
+        public readonly Bearing facing;
+
+        public Path(Vector2 from, int cost, Bearing facing)
+        {
+            this.from = from;
+            this.cost = cost;
+            this.facing = facing;
+        }
+    }
+}

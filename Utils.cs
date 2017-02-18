d8d187f75a35e0b7b8ca317bc22f51755594a3e2
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-02-18 11:29:33 -0800

    [+] utility function: digit counter

---
+++ Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KevinDOMara.SDSU.CS657.Assignment1
+{
+    /// <summary>
+    /// A container class for generalized utility functions.
+    /// </summary>
+    public static class Utils
+    {
+        /// <summary>
+        /// Finds the number of digits in a number.
+        /// </summary>
+        public static int digitsIn(int n)
+        {
+            return n == 0 ? 1 : (int)(Math.Log10(n)) + 1;
+        }
+    }
+}

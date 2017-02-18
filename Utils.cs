19679fcabcd980028108a7136b160769d1fb6168
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-02-18 13:20:38 -0800

    [+] random string generator

--- Utils.cs
+++ Utils.cs
@@ -8,12 +8,37 @@ namespace KevinDOMara.SDSU.CS657.Assignment1
     /// </summary>
     public static class Utils
     {
+        public const string upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public const string lowerChars = "abcdefghijklmnopqrstuvwxyz";
+        public const string alphaChars = "0123456789";
+        public const string hexChars = "0123456789abcdef";
+
+        private static Random random = new Random();
+
         /// <summary>
         /// Finds the number of digits in a number.
         /// </summary>
         public static int digitsIn(int n)
         {
             return n == 0 ? 1 : (int)(Math.Log10(n)) + 1;
+        }
+
+        /// <summary>
+        /// Creates an randomized string consisting of characters from the 
+        /// provided character pool.
+        /// </summary>
+        /// <param name="charSet">Set of characters to randomize between.
+        /// </param>
+        public static string randomString(string charSet, int length)
+        {
+            string randStr = "";
+
+            for (int i = 0; i < length; ++i)
+            {
+                randStr += charSet[random.Next(charSet.Length)];
+            }
+
+            return randStr;
         }
     }
 }

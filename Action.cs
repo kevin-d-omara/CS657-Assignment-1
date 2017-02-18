c7a3e1812db066f9859d11293c47790aee30fe7f
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-02-18 11:27:16 -0800

    [+] Start action & ToString() method

--- Action.cs
+++ Action.cs
@@ -5,7 +5,7 @@ namespace KevinDOMara.SDSU.CS657.Assignment1
 {
     public class Action
     {
-        public enum Type { Move, Rotate, Revert, Wait }
+        public enum Type { Move, Rotate, Revert, Wait, Start }
 
         public Type type;
         public Direction direction;
@@ -13,6 +13,25 @@ namespace KevinDOMara.SDSU.CS657.Assignment1
         {
             this.type = type;
             this.direction = direction;
+        }
+
+        public override string ToString()
+        {
+            switch (type)
+            {
+                case Action.Type.Move:
+                    return "Move";
+                case Action.Type.Rotate:
+                    return "Rotate";
+                case Action.Type.Revert:
+                    return "Revert";
+                case Action.Type.Wait:
+                    return "Wait";
+                case Action.Type.Start:
+                    return "Start";
+                default:
+                    return "!Undefined!";
+            }
         }
     }
 }
